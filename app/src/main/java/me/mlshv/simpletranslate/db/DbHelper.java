dc416eb478555c3dd9ec79a26ca18f26c2ba1ae5
Mikhail Malyshev <mlshv42@yandex.ru>
2017-04-15 01:26:41 +0300

    Добавлены классы для работы с БД

---
+++ app/src/main/java/me/mlshv/simpletranslate/db/DbHelper.java
@@ -0,0 +1,50 @@
+package me.mlshv.simpletranslate.db;
+
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DbHelper extends SQLiteOpenHelper {
+    private static final String DB_NAME = "simple_translate.db";
+    private static final int DB_VERSION = 1;
+
+    // Таблицы для истории и избранного
+    public static final String HISTORY_TABLE = "history";
+    public static final String FAVORITES_TABLE = "favorites";
+
+    // Колонки таблиц
+    public static final String _ID = "_id";
+    public static final String SOURCE_STRING = "word";
+    public static final String TRANSLATION = "translation";
+    public static final String VARIATIONS = "variations";
+
+    private static final String CREATE_HISTORY_TABLE = "create table " + HISTORY_TABLE + "("
+            + _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + SOURCE_STRING + " TEXT NOT NULL UNIQUE, "
+            + TRANSLATION + " TEXT, "
+            + VARIATIONS + " TEXT);";
+
+    private static final String CREATE_FAVORITES_TABLE = "create table " + FAVORITES_TABLE + "("
+            + _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+            + SOURCE_STRING + " TEXT NOT NULL UNIQUE, "
+            + TRANSLATION + " TEXT, "
+            + VARIATIONS + " TEXT);";
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_HISTORY_TABLE);
+        sqLiteDatabase.execSQL(CREATE_FAVORITES_TABLE);
+    }
+
+    public DbHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + HISTORY_TABLE);
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + FAVORITES_TABLE);
+        onCreate(sqLiteDatabase);
+    }
+}
---
+++ app/src/main/java/me/mlshv/simpletranslate/db/DbManager.java
@@ -0,0 +1,67 @@
+package me.mlshv.simpletranslate.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+
+import me.mlshv.simpletranslate.model.Translation;
+
+public class DbManager {
+    private DbHelper dbHelper;
+    private Context context;
+    private SQLiteDatabase database;
+
+    public DbManager(Context context) {
+        this.context = context;
+    }
+
+    public DbManager open() throws SQLException {
+        dbHelper = new DbHelper(context);
+        database = dbHelper.getWritableDatabase();
+        return this;
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
+
+    public Cursor fetchTable(String tableName) {
+        String[] columns = new String[] { DbHelper._ID, DbHelper.SOURCE_STRING, DbHelper.TRANSLATION, DbHelper.VARIATIONS };
+        Cursor cursor = database.query(tableName, columns, null, null, null, null, DbHelper._ID + " DESC"); // сортировка по убыванию id
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+        return cursor;
+    }
+
+    public int updateHistory(String sourceString, String translation, String variations) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DbHelper.SOURCE_STRING, sourceString);
+        contentValues.put(DbHelper.TRANSLATION, translation);
+        contentValues.put(DbHelper.VARIATIONS, variations);
+        return database.update(DbHelper.HISTORY_TABLE, contentValues, DbHelper.SOURCE_STRING + " = " + sourceString, null);
+    }
+
+    public void clearHistory() {
+        database.delete(DbHelper.HISTORY_TABLE, null, null);
+    }
+
+    public void saveTranslation(Translation translation, String table) {
+        ContentValues contentValue = new ContentValues();
+        contentValue.put(DbHelper.SOURCE_STRING, translation.getTerm());
+        contentValue.put(DbHelper.TRANSLATION, translation.getTranslation());
+        if (translation.getVariations() != null) {
+            contentValue.put(DbHelper.VARIATIONS, translation.getVariations().getJson());
+        }
+        deleteRowsWhereSourceStringIs(translation.getTerm(), table);
+        database.insert(table, null, contentValue);
+    }
+
+    private void deleteRowsWhereSourceStringIs(String sourceString, String table) {
+        String whereClause = DbHelper.SOURCE_STRING + "=?";
+        String[] whereArgs = new String[] { sourceString };
+        database.delete(table, whereClause, whereArgs);
+    }
+}
